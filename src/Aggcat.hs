7a7643f49f9d9d5c8d3b6cdb5373a40289c8032c
Cary M. Robbins <carymrobbins@gmail.com>
2013-12-01 23:57:18 -0600

    Fixed getCertificate to retrieve private key.

--- src/Aggcat.hs
+++ src/Aggcat.hs
@@ -3,12 +3,13 @@ module Aggcat where
 
 import Control.Exception
 import Control.Monad
+import Crypto.PubKey.OpenSsh
 import qualified Data.Aeson as J
+import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
 import Data.Conduit
 import Data.String.Utils
 import Network.HTTP.Conduit
-import Network.TLS.Extra
 import System.IO
 
 import Aggcat.Types
@@ -50,8 +51,13 @@ keyDirectory = "/home/crobbins/projects/haskell-aggcat/test-files/"
 readKey :: String -> IO String
 readKey = liftM strip . readFile . (keyDirectory ++)
 
---getCertificate :: IO PrivateKey
-getCertificate = fileReadPrivateKey $ "test-files/certificate.key"
+--getCertificate :: IO (Either ErrorMessage PrivateKey)
+getCertificate = do
+    liftM (extract . decodePrivate) $ BS.readFile "test-files/certificate.key"
+  where
+    extract (Right (OpenSshPrivateKeyRsa pkey)) = Right pkey
+    extract (Left e) = (Left e)
+
 
 getConsumerKey :: IO ConsumerKey
 getConsumerKey = readKey "consumer_key"
--- src/Aggcat/Types.hs
+++ src/Aggcat/Types.hs
@@ -4,3 +4,5 @@ type IssuerId = String
 type ConsumerKey = String
 type ConsumerSecret = String
 type CustomerId = String
+type ErrorMessage = String
+
